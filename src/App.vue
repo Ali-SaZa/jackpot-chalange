51b0d05145d79718887914954f3723cfaa1a27aa
ali sagheb zabihi <alizabihi1997@gmail.com>
2022-12-04 10:13:02 +0330

    initial project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import JackpotComponent from './components/JackpotComponent.vue'
+</script>
+
+<template>
+  <div>
+    <JackpotComponent />
+  </div>
+</template>
+
+<style scoped>
+</style>
